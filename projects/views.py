4a13534e05633f84a0505e31b79f79698eb8889b
noordseoostende <voropayev1@mail.ru>
2021-12-13 22:58:20 +0100

    voorbereiding

--- projects/views.py
+++ projects/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def projects(request):
-    return render(request, 'projects/projects.html')
+    page = 'Master cleanse retro brunch keytar dreamcatcher, 90s sriracha hammock.'
+    number = 26
+    context = {'page': page, 'number': number}
+    return render(request, 'projects/projects.html', context)
 
 def project(request, pk):
     return render(request, 'projects/single-project.html')
