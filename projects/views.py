259506c9521e8493811dcd4913ad162e50de512e
noordseoostende <voropayev1@mail.ru>
2021-12-14 22:23:03 +0100

    nice work with url

--- projects/views.py
+++ projects/views.py
@@ -1,14 +1,36 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+projecstList = [
+    {
+        'id':'1',
+        'title':'Webshop',
+        'description':'Website die u wenst te kennen',
+    },
+    {
+        'id':'2',
+        'title':'Dubble',
+        'description':'Wat we probeerden naappen van 2009 projects awards',
+    },
+    {
+        'id':'3',
+        'title':'Hashtags zoeker ',
+        'description':'Wat we probeerden niet doen in 2010',
+    },
+]
+
 def projects(request):
-    page = 'Master cleanse retro brunch keytar dreamcatcher, 90s sriracha hammock.'
+    page = 'Master'
     number = 26
-    context = {'page': page, 'number': number}
+    context = {'page': page, 'number': number, 'projects': projecstList}
     return render(request, 'projects/projects.html', context)
 
 def project(request, pk):
-    return render(request, 'projects/single-project.html')
+    projectObj = None
+    for i in projecstList:
+        if i['id'] == pk:
+            projectObj = i
+    return render(request, 'projects/single-project.html', {'project': projectObj})
     
 
 # Create your views here.
