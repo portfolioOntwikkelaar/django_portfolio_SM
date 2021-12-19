24ee8d6313c89a934c457ab0645370ed0e9d8312
noordseoostende <voropayev1@mail.ru>
2021-12-19 19:34:29 +0100

    functionaliteit en stijl(check)

--- projects/urls.py
+++ projects/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('', views.projects, name="projects"),
     path('project/<str:pk>/', views.project, name="project"),
 
-    path('create-projects/', views.createProject, name="create-project")
+    path('create-projects/', views.createProject, name="create-project"),
+    path('update-project/<str:pk>/', views.updateProject, name="update-project"),
+    path('delete-project/<str:pk>/', views.deleteProject, name="delete-project"),
 ]
