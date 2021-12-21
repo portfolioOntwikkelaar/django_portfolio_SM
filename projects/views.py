3facd064f64bbb8878b06cf3264e743bd998c7a4
noordseoostende <voropayev1@mail.ru>
2021-12-21 21:45:38 +0100

    meld je aan

--- projects/views.py
+++ projects/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 from .models import Project
 from .forms import ProjectForm
 
@@ -10,13 +11,14 @@ def projects(request):
     context = {'projects': projects}
     return render(request, 'projects/projects.html', context)
 
+
 def project(request, pk):
     projectObj = Project.objects.get(id=pk)
     
     return render(request, 'projects/single-project.html', {'project': projectObj})
     
 
-# Create your views here.
+@login_required(login_url="login")
 def createProject(request):
     form = ProjectForm()
     
@@ -31,7 +33,7 @@ def createProject(request):
     context = {'form': form}
     return render(request, "projects/project_form.html", context)
 
-
+@login_required(login_url="login")
 def updateProject(request, pk):
     project = Project.objects.get(id=pk)
     form = ProjectForm(instance=project)
@@ -47,6 +49,7 @@ def updateProject(request, pk):
     context = {'form': form}
     return render(request, "projects/project_form.html", context)
 
+@login_required(login_url="login")
 def deleteProject(request, pk):
     project = Project.objects.get(id=pk)
     if request.method == 'POST':
