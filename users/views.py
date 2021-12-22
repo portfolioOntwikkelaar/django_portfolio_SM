e7064a87162dcf216205bc5662253f01fbfb1fd0
noordseoostende <voropayev1@mail.ru>
2021-12-22 21:42:56 +0100

    het inloggen fnc

--- users/views.py
+++ users/views.py
@@ -2,11 +2,13 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Profile
 
 
 def loginUser(request):
+  page = 'login'
 
   if request.user.is_authenticated:
     return redirect('profiles')
@@ -34,6 +36,26 @@ def logoutUser(request):
   messages.error(request, 'User was logged out')
   return redirect('login')
 # Create your views here.
+
+def registerUser(request):
+  page = 'register'
+  form = UserCreationForm()
+
+  if request.method == 'POST':
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      user = form.save(commit=False)
+      user.username = user.username.lower()
+      user.save()
+
+      messages.success(request, 'User account was created!')
+
+      login(request, user)
+      return redirect('profiles')
+    else:
+      messages.success(request, 'An error has occurred during registration!')
+  context = {'page':page, 'form':form}
+  return render(request, 'users/login_register.html', context)
 
 
 def profiles(request):
